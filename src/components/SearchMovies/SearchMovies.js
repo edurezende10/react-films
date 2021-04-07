c88fd15ef1a633c298cfb15dadd5f7b9ede5b1bc
eduardo rezende cruz <edu_rezende11@hotmail.com>
2021-04-07 15:57:52 -0300

    estilizando o compononente

--- src/components/SearchMovies/SearchMovies.js
+++ src/components/SearchMovies/SearchMovies.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import './SearchMovies.css'
+import './SearchMovies.css';
 
 const SearchMovies = () => {
+  const searchMovies = async (event) => {
+    event.preventDefault();
+
+    const query = 'Jurassic Park';
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=82acc287e0747c78c3079b7e1907472a&language=pt-BR&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      console.log(data);
+    } catch(err){
+        console.error(err)
+    }
+  };
+
   return (
     <>
-      <form action="" className="form">
+      <form onSubmit={searchMovies} className="form">
         <label htmlFor="query" className="label">
           Movie Name
         </label>
