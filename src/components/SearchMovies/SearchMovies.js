9fc58a767f80ff0f3d60418dec9bce1ecc91d385
eduardo rezende cruz <edu_rezende11@hotmail.com>
2021-04-07 21:27:02 -0300

    adicionando na tela as informações do filme

--- src/components/SearchMovies/SearchMovies.js
+++ src/components/SearchMovies/SearchMovies.js
@@ -4,26 +4,25 @@ import './SearchMovies.css';
 const SearchMovies = () => {
   /* vamos usar o state, para mudar a constante query, que inicialmente será vazio e posteriormente sera adicionado o valor que o usuario definiu no input */
 
-  const [query,setQuery]= React.useState('')
+  const [query, setQuery] = React.useState('');
   /* definimos o valor inicial como uma string vazia no use state */
 
-
-  const [movies,setMovies]=React.useState([])
+  const [movies, setMovies] = React.useState([]);
 
   const searchMovies = async (event) => {
     event.preventDefault();
 
-/*     const query = 'Jurassic Park';
- */
+    /*     const query = 'Jurassic Park';
+     */
     const url = `https://api.themoviedb.org/3/search/movie?api_key=82acc287e0747c78c3079b7e1907472a&language=pt-BR&query=${query}&page=1&include_adult=false`;
 
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setMovies(data.results)
+      setMovies(data.results);
       console.log(movies);
-    } catch(err){
-        console.error(err)
+    } catch (err) {
+      console.error(err);
     }
   };
 
@@ -40,13 +39,39 @@ const SearchMovies = () => {
           placeholder="Fight club "
           id="query"
           value={query}
-          onChange={(event)=>{setQuery(event.target.value)}}
+          onChange={(event) => {
+            setQuery(event.target.value);
+          }}
         />
         {/* no input definimos o valor inicial, no metodo onChange, que captura quando há uma tentativa de mudança do valor, passamos uma arrow function , chamando a função setQuery e passando o valor que está no input como parametro da query */}
         <button className="button" type="submit">
           Search
         </button>
       </form>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <div className="card">
+              <img
+                className="card--image"
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                alt={`${movie.title} poster`}
+                key={movie.id}
+              />
+              <div className="card--content">
+                <h3 className="card--title">{movie.title}</h3>
+                <p>
+                  <small>Data de lançamento: {movie.release_date}</small>
+                </p>
+                <p>
+                  <small>Avaliação: {movie.vote_average}</small>
+                </p>
+                <p className="card--desc">{movie.overview}</p>
+              </div>
+            </div>
+          ))}
+      </div>
     </>
   );
 };
